7802745798ce957a41287fc0f84374f37410361f
Bijil Subhash <bijil.subhash@gmail.com>
2024-09-26 13:49:54 +1000

    comman line arguments part 2

--- extras/example10.py
+++ extras/example10.py
@@ -6,6 +6,17 @@ group = parser.add_mutually_exclusive_group()
 group.add_argument('-v', '--verbose', action='store_true')
 group.add_argument('-q', '--quiet', action='store_true')
 
+parser.add_argument('x', type=int, help='the base')
+
 args = parser.parse_args()
 
 print(args)
+
+if args.quiet:
+    print('quiet mode...')
+    print('nothing to see here...')
+
+if args.verbose:
+    print('verbose mode...')
+    print('this is the base:', args.x)
+
