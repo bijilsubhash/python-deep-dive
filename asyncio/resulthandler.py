42989ff75b8f9c98dfefeeba403b42a53538a41c
Bijil Subhash <bijil.subhash@gmail.com>
2025-02-01 17:11:11 +1100

    asycnio

---
+++ asyncio/resulthandler.py
@@ -0,0 +1,11 @@
+import asyncio
+from typing import Callable
+
+async def handle_task_result(
+        result_queue: asyncio.Queue,
+        callback: Callable[[dict], None]
+) -> None:
+    while True:
+        result = await result_queue.get()
+        callback(result)
+        result_queue.task_done()
